43ae9c9ef0a547ea45229f2091b5f1da7c66493e
shivam21bb106 <shivam21bbbb@gmail.com>
2025-09-13 15:20:15 +0530

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './components/Auth/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <App />
+
+      <Login />
     </>
   )
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 const Login = () => {
+
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log("Form Submitted")
+        setEmail('')
+        setPassword('')
+    }
     return (
         <>
 
-            <div className="flex items-center justify-center">
-                <div className="border-2 border-red-600">
+            <div className="flex h-screen w-screen items-center justify-center">
+                <div className="border-2 rounded-xl border-emerald-600 p-20">
 
 
-                    <form>
-                        <input type="email" />
+                    <form onSubmit={(e) => { submitHandler(e) }} className="flex  flex-col items-center justify-center" >
+                        <input className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type=" email" required placeholder="Enter your name" onChange={(e) => setEmail(e.target.value)} />
+                        <input className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" required placeholder="Enter your Password" onChange={(e) => setPassword(e.target.value)} />
+                        <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white' type="sbmit">Submit</button>
+
                     </form>
                 </div>
 
-            </div>
+            </div >
         </>
     )
 
