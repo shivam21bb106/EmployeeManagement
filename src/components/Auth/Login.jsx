9de2e5f3cbbfcbaf596dc15aa77f35d2f9000118
shivam21bb106 <shivam21bbbb@gmail.com>
2025-09-13 12:31:52 +0530

    initial commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Login = () => {
+    return (
+        <>
+
+            <div className="flex items-center justify-center">
+                <div className="border-2 border-red-600">
+
+
+                    <form>
+                        <input type="email" />
+                    </form>
+                </div>
+
+            </div>
+        </>
+    )
+
+}
+export default Login
